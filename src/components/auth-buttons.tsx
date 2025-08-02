0c4840ff3eb96d76ebcc54b50c982902448f054e
S1DDHEY <siddheyp@gmail.com>
2025-08-02 17:53:00 +0530

    added user auth

---
+++ src/components/auth-buttons.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import Link from "next/link";
+import { useAuth } from "../../context/auth"
+
+export default function AuthButtons() {
+  const auth = useAuth();
+
+  return (
+    <div>
+      {!!auth?.currentUser &&
+      <>
+      <div>{auth.currentUser.email}</div>
+      <div onClick={() => {
+        auth.logout();
+      }}
+      >
+        Logout
+        </div>
+      </>
+      }
+      {!auth?.currentUser &&
+        <>
+          <Link href="/login">Login</Link>
+          <Link href="/register">SignUp</Link>
+        </>
+      }
+    </div>
+  );
+}
